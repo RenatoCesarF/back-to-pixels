41ce6b66716eecf2ce1fc51d780063c7f77dd198
Renato Cesar <62253156+RenatoCesarF@users.noreply.github.com>
2022-01-02 16:15:47 -0400

    :iphone: Responsive design in blog page

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,11 +7,20 @@ interface IPost{
 }
 
 const PostCard: React.FC<IPost> = ({post}: IPost) => {
-    //
+    const hasCoverImage:boolean = post.cover_image != undefined
     return(
         <Link href={`/blog/${post.slug}`}>
+            
             <div className='post-card-div'>
-                    <img className='post-card-img' src={post.cover_image}/>
+                    <div className='post-card-image-container'>
+
+                    {
+                        hasCoverImage === true ? 
+                        (<img className='post-card-img' src={post.cover_image}/>)
+                        : 
+                        (<h1>aaaa{post.date}</h1>)
+                    }
+                    </div>
                     <div className='post-card-container'>
                         <h4 className='post-card-title'>{post.title}</h4>
                         <p className='post-card-resume'>{post.excerpt}</p>
