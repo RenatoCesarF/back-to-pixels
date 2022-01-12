34064adf0458c8a900cbfc0cf02be52774ed00a1
Renato Cesar <62253156+RenatoCesarF@users.noreply.github.com>
2022-01-12 13:31:10 -0400

    :rocket: Change sitemap generator to next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+const siteUrl = 'http://localhost:3000'
+
+module.exports = {
+    siteUrl,
+    generateRobotsTxt: true,
+};
--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./scripts/sitemap-generator");
-    }
-    return config;
-  },
+
 }
