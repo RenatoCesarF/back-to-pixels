3a6e5514a593cf7edccc96275ae11b952d12ee91
Renato Cesar <62253156+RenatoCesarF@users.noreply.github.com>
2022-02-14 15:48:15 -0400

    💄 Improve dark theme colors

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -42,7 +42,6 @@ export default function Header() {
                 </Link>
                 
                 <div className='menu-icon-div'>
-                    {/* <ThemeSwitch/> */}
                     <div onClick={(toggleMenuIcon)}>
                         <MenuIcon/>
                     </div>
