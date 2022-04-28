2c182232d39f4e655d813a051e19089344e1642b
Renato Cesar <62253156+RenatoCesarF@users.noreply.github.com>
2022-04-28 13:58:12 -0400

    :construction: Bringing styles to global css

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import { FaRegCalendarAlt } from 'react-icons/fa';
 import Link from 'next/link';
 import NextImage from 'next/image'
+import { m } from 'framer-motion';
 
 
 import CategoryTag, { CategoryTagTransparent } from '@components/CategoryTag/CategoryTag';
 import Post from '@classes/Post';
 import Category from '@classes/category';
+import { cardVariants } from '@helpers/animations';
 import { rgbDataURL } from '@utils/rgbDataURL';
 
-import globalStyles from './PostCard.styles'
+// import globalStyles from './PostCard.styles'
 
 interface IPost{
     post: Post
@@ -29,11 +31,11 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
 
     return(
         <>
-            <style jsx global>
+            {/* <style jsx global>
                 {globalStyles}
-            </style>
-            <div className='post-card-expanded-external'>
-                <div className='post-card-div'  
+            </style> */}
+            <m.div variants={cardVariants}  layout className='post-card-expanded-external'>
+                <m.div className='post-card-div'  
                     onMouseOver={()=>{setIsHovering(true)}}
                     onMouseLeave={()=>{setIsHovering(false)}}
                     onTouchCancel={()=>{setIsHovering(false)}}
@@ -41,8 +43,8 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
                     onTouchCancelCapture={()=>{setIsHovering(false)}}
                     >
                     <Link passHref href={`/blog/${post.slug}`}>
-                        <article className="post-card-clickable">
-                            <header className='post-card-image-container'>
+                        <m.article className="post-card-clickable">
+                            <m.header className='post-card-image-container'>
                                 <NextImage 
                                     src={post.cover_image} 
                                     alt='post card image' 
@@ -58,7 +60,7 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
                                         <h1 className='post-card-cover-date'>{post.date}</h1> 
                                     : null 
                                 }
-                            </header>
+                            </m.header>
 
                             <h2 className='post-card-title'>{post.title}</h2>
 
@@ -70,10 +72,10 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
                                 </div>
                             
                             </div>
-                        </article>
+                        </m.article>
                     </Link>
 
-                    <footer className='post-card-footer'>
+                    <m.footer className='post-card-footer'>
                         <div className='card-date-row'>
                             <FaRegCalendarAlt id='card-date-icon' size={16}/>
                             <p className="post-card-date">{post.date}</p>
@@ -97,12 +99,13 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
                                         {
                                             post.categories.map((category: Category, index: number)=>{
                                                 return (
-                                                    <div 
+                                                    <m.div 
+                                                        whileHover={{ scale: 1.1 }}
                                                         style={{display: "inline-block", transitionDuration: "0s"}}
                                                         key={index}  
                                                     >
                                                         <CategoryTag category={category} key={index} />
-                                                    </div>
+                                                    </m.div>
                                                 )
                                             })
                                         }
@@ -113,9 +116,9 @@ const PostCard: React.FC<IPost> = ({post}: IPost) => {
                                 }
                             </div>
                         </div>
-                    </footer>
-                </div>
-            </div>
+                    </m.footer>
+                </m.div>
+            </m.div>
         </>
     )
 }
